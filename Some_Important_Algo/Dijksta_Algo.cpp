c3e34a71b07bda745fe45b3d499da7ee85f206b0
Yash Gautam <35126822+yashgautam1221@users.noreply.github.com>
2020-10-01 13:06:42 +0530

    Update Dijksta_Algo.cpp

--- Some_Important_Algo/Dijksta_Algo.cpp
+++ Some_Important_Algo/Dijksta_Algo.cpp
@@ -1,59 +1,76 @@
-#include<bits/stdc++.h>
-
+#include <bits/stdc++.h>
 using namespace std;
+#define forInc(i, a, b) for (ll i = a; i <= b; i++)
+#define forDec(i, b, a) for (ll i = b; i >= a; i--)
+#define inf LLONG_MAX
+#define neginf LLONG_MIN
+#define mod 1000000007
+#define eps 1e-9
+typedef long long ll;
+typedef unsigned long long ull;
 
-#define pb push_back
-
-void Dijkstra(int source, int n, vector<pair<int, int>> &v(1001),int &dis[1001],bool &vis[1001])
+vector<ll> dis, parent;
+vector<vector<ll>> graphedge, graphcost;
+ll vertex, edges;
+void dijkstra(ll source)
 {
-    for (int i = 0; i < 1001; i++)
-        dis[i] = INT_MAX;
-    class prioritize
-    {
-    public:
-        bool operator()(pair<int, int> &p1, pair<int, int> &p2) {
-            return p1.second > p2.second; 
-        }
-    };
-    priority_queue<pair<int, int>, vector<pair<int, int>>, prioritize> pq;
-    pq.push(make_pair(source, dis[source] = 0));
+    dis[source] = 0;
+    parent[source] = source;
+    priority_queue<pair<ll, ll>, vector<pair<ll, ll>>, greater<pair<ll, ll>>> pq;
+    pq.push({dis[source], source});
     while (!pq.empty())
     {
-        pair<int, int> curr = pq.top();
+        pair<ll, ll> node = pq.top();
         pq.pop();
-        int cv = curr.first, cw = curr.second;
-        if (vis[cv])
-            continue;
-        vis[cv] = true;
-        for (int i = 0; i < v[cv].size(); i++)
-            if (!vis[v[cv][i].first] && v[cv][i].second + cw < dis[v[cv][i].first])
-                pq.push(make_pair(v[cv][i].first, (dis[v[cv][i].first] = v[cv][i].second + cw)));
+        if (graphedge[node.second].size())
+        {
+            forInc(i, 0, graphedge[node.second].size() - 1)
+            {
+                ll child = graphedge[node.second][i];
+                ll edgecost = graphcost[node.second][i];
+                if (edgecost + dis[node.second] < dis[child])
+                {
+                    dis[child] = edgecost + dis[node.second];
+                    parent[child] = node.second;
+                    pq.push(make_pair(dis[child], child));
+                }
+            }
+        }
     }
 }
 
-
 int main()
 {
-    vector<pair<int, int>> v(1001); 
-    int dis[1001];                  
-    bool vis[1001] = {0};
-    int n, m, x, y, w;
-    cout << "Enter number of vertices and edges in the graph\n";
-    cin >> n >> m;
-    for (int i = 0; i < m; i++)
+    cout << "Enter number of vertex:\n";
+    cin >> vertex;
+    cout << "Enter number of edges\n";
+    cin >> edges;
+    ll node1, node2, weight;
+    dis.clear();
+    parent.clear();
+    graphedge.clear();
+    graphcost.clear();
+    graphedge.resize(vertex + 5);
+    graphcost.resize(vertex + 5);
+    dis.resize(vertex + 5, inf);
+    parent.resize(vertex + 5, -1);
+    cout << "Enter vertex1,vertex2 and weight\n";
+    forInc(i, 1, edges)
     {
-        cin >> x >> y >> w;
-        v[x].pb(make_pair(y, w));
-        v[y].pb(make_pair(x, w));
+        cout << "Edge " << i << endl;
+        cin >> node1 >> node2 >> weight;
+        graphedge[node1].push_back(node2);
+        graphcost[node1].push_back(weight);
+        graphedge[node2].push_back(node1);
+        graphcost[node2].push_back(weight);
     }
-    int s;
-    cin >> s;
-    Dijkstra(s, n,v,dis,vis);
-    cout << "Source is: " << s << ". The shortest distance to every other vertex from here is: \n";
-    for (int i = 1; i <= n; i++)
-    {
-        cout << "Vertex: " << i << " , Distance: ";
-        dis[i] != INT_MAX ? cout << dis[i] << "\n" : cout << "-1\n";
-    }
+
+    cout << "Source Vertex : " << endl;
+    ll source;
+    cin >> source;
+    dijkstra(source);
+    cout << "VERTEX\tCOST\tPARENT" << endl;
+    forInc(i, 1, vertex) cout << i << " \t" << dis[i] << "\t" << parent[i] << endl;
+    cout << endl;
     return 0;
 }
